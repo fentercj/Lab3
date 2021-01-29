76071f2fee826f1b9e634e8fe7561abbaa3ad006
fentercj <fentercj@mail.uc.edu>
2021-01-29 18:55:54 +0000

    completed Lab 3

--- Main.java
+++ Main.java
@@ -1,8 +1,26 @@
 import java.util.Scanner;
+
 class Main {
-  Scanner s = new Scanner(system.in);
   public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
     System.out.println("Enter a number to count down or up from: ");
-  int number = s.nextInt();
+    
+    int number = s.nextInt();
+    
+    if(number > 0){
+      while (number > 0){
+        System.out.println(number - 1);
+        number--;
+    }}
+    else{
+      while (number < 0){
+        System.out.println(number + 1);
+        number++;
+    }}
+    
+    System.out.println("Blast off!");
+
   }
+  
+
 }
