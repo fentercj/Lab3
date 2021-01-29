b7cbd7abf9da53810ee38553983fdea7c6304337
fentercj <fentercj@mail.uc.edu>
2021-01-29 19:10:14 +0000

    Cleaned it up. Now final!

--- Main.java
+++ Main.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Scanner s = new Scanner(System.in);
+    
     System.out.println("Enter a number to count down or up from: ");
     
     int number = s.nextInt();
     
-    if(number > 0){
+    if(number >= 0){
       while (number > 0){
         System.out.println(number - 1);
         number--;
