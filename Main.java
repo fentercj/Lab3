67fa9b7f2809822b1842ce0677615eb3085d3c48
fentercj <fentercj@mail.uc.edu>
2021-01-29 18:24:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+class Main {
+  Scanner s = new Scanner(system.in);
+  public static void main(String[] args) {
+    System.out.println("Enter a number to count down or up from: ");
+  int number = s.nextInt();
+  }
+}
